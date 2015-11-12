c898d0bf3a12a8d0ca3325d0da8efc09a4ca7da6
zbq <zbq@163.com>
2015-11-12 17:24:33 +0800

    123

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -1,8 +1,11 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
+
+class IndexController extends Controller 
+{
+    public function index()
+    {
     	import("Common.Util.RedisPool");
     	$redis = RedisPool::getConnect();
     	$redis->SET('uname', 'zimu');
