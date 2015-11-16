dbd38c3593528554e88f7a61f41a4a15c89a1d0c
zbq <zbq@163.com>
2015-11-16 09:53:56 +0800

    changelanguage

--- Apps/Home/Controller/PublicController.class.php
+++ Apps/Home/Controller/PublicController.class.php
@@ -8,11 +8,11 @@ Class PublicController extends BaseController
      * @return [type] [description]
      */
     public function getBaseLanguage() {
-        $this->return['data']['language'] = F('baseLanguage');
+        //$this->return['data']['language'] = F('baseLanguage');
 
         if(!$this->return['data']['language']) {
             $this->return['data']['language'] = D('language')->where('type=1')->field('lid, language_name')->select();
-            F('baseLanguage', $this->return['data']);
+            F('baseLanguage', $this->return['data']['language']);
         }
 
         $this->goJson($this->return);
