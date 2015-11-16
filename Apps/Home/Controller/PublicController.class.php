d8e142450e852094492402c41c51597da8297e4a
zbq <zbq@163.com>
2015-11-16 10:03:17 +0800

    12

--- Apps/Home/Controller/PublicController.class.php
+++ Apps/Home/Controller/PublicController.class.php
@@ -8,7 +8,7 @@ Class PublicController extends BaseController
      * @return [type] [description]
      */
     public function getBaseLanguage() {
-        //$this->return['data']['language'] = F('baseLanguage');
+        $this->return['data']['language'] = F('baseLanguage');
 
         if(!$this->return['data']['language']) {
             $this->return['data']['language'] = D('language')->where('type=1')->field('lid, language_name')->select();
