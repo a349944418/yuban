747d6427e348b2d804bc15d9cb324a82f22e59b0
zbq <zbq@163.com>
2015-11-24 10:03:46 +0800

    官网首页

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -6,29 +6,15 @@ class IndexController extends BaseController
 {
     public function index()
     {
-    	// import("Common.Util.RedisPool");
-    	// $redis = \RedisPool::getConnect();
-    	//$redis->SET('uname', 'zimu');
-        // echo $this->redis->FLUSHALL();
-        // echo '<br />';
-        // $arr = array('achat_score'=>1, 'vchat_score'=>2);
-        // $this->redis->SET('score_setting', json_encode($arr));
-        // dump($this->redis->GET('score_setting'));
-    	// echo $this->redis->GET('Token:uid2');
-    	// echo '<br />';
-         
-    	 dump($this->redis->HGETALL('Userinfo:uid2'));
-        //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+    	
+        //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;text-align:center"> <p>欢迎访问 <b>语加</b>官网！</p><br/>网站正在开发中！！！</div>','utf-8');
     
 
 	}
 
     public function test()
     {
-	    $res = $this->redis->SADD('test', 1);
-        dump($res);
-        $res1 = $this->redis->Sadd('test', 2);
-        dump($res1);
-        dump($this->redis->Sadd('test', 1));
+        // echo $this->redis->FLUSHALL();
+        dump($this->redis->get('Token:uid3'));
     }
 }
